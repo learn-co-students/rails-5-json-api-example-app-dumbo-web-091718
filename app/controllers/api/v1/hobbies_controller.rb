2da0eca9a8db7bbe1a3e84a561a58be2a46f8282
github username <github email address>
2018-11-09 14:32:09 -0500

    done.

--- app/controllers/api/v1/hobbies_controller.rb
+++ app/controllers/api/v1/hobbies_controller.rb
@@ -5,7 +5,7 @@ module Api
       def index
         # all hobby objs loaded with their cats vs. 1 query per hobby
         # do both and take pic of terminal
-        # render json: Hobby.all, include: ['cats']
+        # render json: Hobby.all, include: ['cats'].
         render json: Hobby.includes(:cats), include: ['cats']
       end
 
